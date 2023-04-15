a85822e8d322fd9d4af10622e339a8528a74d027
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-15 23:59:15 +0530

    updating

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "spot-ec2" {
   count                     = var.SPOT_INSTANCE_COUNT
   ami                       = data.aws_ami.image.id
   instance_type             = var.INSTANCE_TYPE
-  subnet_id                 = element(data.terraform_remote_state.vpc.PRIVATE_SUBNET_IDS, count.index)
+  subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
   vpc_security_group_ids    = [aws_security_group.sg.id]
   wait_for_fulfillment      = true
 
@@ -18,7 +18,7 @@ resource "aws_instance" "od-ec2" {
   count                   = var.OD_INSTANCE_COUNT
   ami                     = data.aws_ami.image.id
   instance_type           = var.INSTANCE_TYPE
-  subnet_id               = element(data.terraform_remote_state.vpc.PRIVATE_SUBNET_IDS, count.index)
+  subnet_id               = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
   vpc_security_group_ids  = [aws_security_group.sg.id]
 
   tags = {
