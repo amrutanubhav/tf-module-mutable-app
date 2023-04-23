5f46aa256a28f6464821d41d43957c78218ae7bb
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-23 12:45:35 +0530

    updating

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tf-module-mutable-app
+
+common backend module for all backend components
+
+strategy is as below:
+>create ec2 instance <half spot and half on demand>
+>Install application component inside it <ansible playbook>
+>create target group
+>attach TG to an ALB
+>DNS record to ALB
---
+++ target-group.tf
@@ -0,0 +1,35 @@
+#creates TG
+resource "aws_lb_target_group" "app-tg" {
+  name        = "${var.COMPONENT}-${var.ENV}"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+##Attaching the instances to the created TG
+resource "aws_lb_target_group_attachment" "instanc-attach" {
+  count            = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
+  target_group_arn = aws_lb_target_group.app-tg.arn
+  target_id        = element(local.ALL_INSTANCE_IDS, count.index)
+  port             = 8080
+}
+
+#backend components TG need to be attached to private alb and frontend TG needs to be attached to public alb
+
+# adding rules to the target group
+
+resource "aws_lb_listener_rule" "app-rule" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-tag.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}- ${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"]
+    }
+  }
+}
