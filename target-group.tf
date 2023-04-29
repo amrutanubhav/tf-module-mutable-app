6578d581b2aaad50cfcd14f002d9951c68f5882e
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-29 19:40:23 +0530

    updating

--- target-group.tf
+++ target-group.tf
@@ -11,14 +11,22 @@ resource "aws_lb_target_group_attachment" "instanc-attach" {
   count            = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
   target_group_arn = aws_lb_target_group.app-tg.arn
   target_id        = element(local.ALL_INSTANCE_IDS, count.index)
-  port             = 8080
+  port             = var.PORT
 }
 
 #backend components TG need to be attached to private alb and frontend TG needs to be attached to public alb
 
-# adding rules to the target group
+#generates random priority
 
-resource "aws_lb_listener_rule" "app-rule" {
+resource "random_integer" "priority" {
+  min = 100
+  max = 800
+}
+
+#adding rules to the created public target group
+
+resource "aws_lb_listener_rule" "private-tg-rule" {
+  count        = var.ALB_TYPE == "internal" ? 1 : 0
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = random_integer.priority.result
 
@@ -34,9 +42,16 @@ resource "aws_lb_listener_rule" "app-rule" {
   }
 }
 
-#generates random priority
 
-resource "random_integer" "priority" {
-  min = 100
-  max = 800
+#create listener for public LB
+resource "aws_lb_listener" "public" {
+  count             = var.ALB_TYPE == "internal" ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-tg.arn
+  }
 }
